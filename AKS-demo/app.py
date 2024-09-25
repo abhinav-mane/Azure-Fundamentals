ff12248d26f05e8919af724aa200d9f813a372ad
Abhinav Mane <maneabh1217@gmail.com>
2024-09-25 18:32:27 +0530

    Update app.py

--- AKS-demo/app.py
+++ AKS-demo/app.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/predict")
 def predict():
-    responses = ["sonam weds abhinav", "abhinav weds sonam", "he weds she","she weds he"]
+    responses = ["yes", "no", "dont know"]
     return {"prediction": random.choice(responses)}
